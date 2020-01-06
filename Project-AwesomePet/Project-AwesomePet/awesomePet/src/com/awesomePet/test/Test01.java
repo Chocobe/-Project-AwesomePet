751dd42f037d3c707c1bd37323e1e64874450db2
Chocobe <nice_10bird@naver.com>
2020-01-06 16:41:40 +0900

    <master> : Created test files

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/test/Test01.java
@@ -0,0 +1,29 @@
+package com.awesomePet.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test01/")
+public class Test01 extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		String contextPath = request.getContextPath();
+		String servletPath = request.getServletPath();
+		String pathInfo = request.getPathInfo();
+		
+		out.print("<h1>Context Path : " + contextPath + "</h1>");
+		out.print("<h1>Servlet Path : " + servletPath + "</h1>");
+		out.print("<h1>Path Info : " + pathInfo + "</h1>");
+		
+		out.close();
+	}
+}
