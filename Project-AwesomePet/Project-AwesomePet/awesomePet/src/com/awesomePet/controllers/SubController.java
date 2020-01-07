15d2fc211a506eb8f685ce24ad91ab91f0a749e4
Chocobe <nice_10bird@naver.com>
2020-01-07 16:39:22 +0900

    <master> : Fixed SubController to "interface" from "abstrace class"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/SubController.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/SubController.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public abstract class SubController {
+public interface SubController {
 	abstract public void execute(HttpServletRequest request, HttpServletResponse response) 
 					throws ServletException, IOException;
 }
