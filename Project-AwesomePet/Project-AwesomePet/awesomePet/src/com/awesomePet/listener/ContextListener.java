ada797dc4b7df88569b60fc65cbe1202fe811745
Chocobe <nice_10bird@naver.com>
2020-01-07 16:42:16 +0900

    <master> : Fixed "ContextListener.java"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/listener/ContextListener.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/listener/ContextListener.java
@@ -7,7 +7,9 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import com.awesomePet.controllers.SubController;
-import com.awesomePet.controllers.indexControllers.IndexController;
+import com.awesomePet.controllers.indexControllers.IndexViewController;
+import com.awesomePet.controllers.memberControllers.LoginViewController;
+import com.awesomePet.controllers.memberControllers.MemberLoginController;
 
 public class ContextListener implements ServletContextListener {
 	// 요청에 따른 SubController의 DI(의존성 주입)을 수행합니다.
@@ -15,9 +17,12 @@ public class ContextListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent event) {
 		Map<String, SubController> subControllers = new HashMap<String, SubController>();
 		
-		// index.do 처리를 위한 SubController
-		subControllers.put("/index.do", new IndexController());
+		// index.do 처리를 위한 SubController 입니다.
+		subControllers.put("/index.do", new IndexViewController());
 		
+		// 회원 서비스를 위한 SubController 입니다.
+		subControllers.put("/loginView.do", new LoginViewController());
+		subControllers.put("/memberLogin.do", new MemberLoginController());
 		
 		
 		event.getServletContext().setAttribute("subControllers", subControllers);
