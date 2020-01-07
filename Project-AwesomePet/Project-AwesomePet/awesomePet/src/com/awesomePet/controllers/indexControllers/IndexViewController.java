cc8630fa6d8de31a52610215a7f0f13ab907a792
Chocobe <nice_10bird@naver.com>
2020-01-07 16:41:27 +0900

    <master> : Created class "IndexViewController.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/indexControllers/IndexViewController.java
@@ -0,0 +1,20 @@
+package com.awesomePet.controllers.indexControllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.awesomePet.controllers.ControllerUtil;
+import com.awesomePet.controllers.SubController;
+
+public class IndexViewController implements SubController {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		String path = "/views/index.jsp";
+		ControllerUtil.forward(request, response, path);
+	}
+}
