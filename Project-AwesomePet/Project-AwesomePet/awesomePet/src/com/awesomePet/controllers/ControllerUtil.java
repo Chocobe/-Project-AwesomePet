85d41d82fee0fb9b33cafe8cec1d655c69ebb778
Chocobe <nice_10bird@naver.com>
2020-01-13 19:34:59 +0900

    <master> : Added method "redirect()" in "ControllerUtil.java"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/ControllerUtil.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/ControllerUtil.java
@@ -14,4 +14,11 @@ public class ControllerUtil {
 		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
 		dispatcher.forward(request, response);
 	}
+	
+	
+	// SubController 객체에서 redirect를 위한 유틸 메서드 입니다.
+	public static void redirect(HttpServletResponse response, String path) 
+					throws ServletException, IOException {
+		response.sendRedirect(path);
+	}
 }
