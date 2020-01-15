f8409c136bec84fb4c102ebb130f84d259e888c0
Chocobe <nice_10bird@naver.com>
2020-01-15 16:18:01 +0900

    <master> : Created js "updateMyInfoResultView.js"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/WebContent/js/member/updateMyInfoResultView.js
@@ -0,0 +1,10 @@
+// 홈버튼 메서드 입니다.
+function goToHome(context) {
+    location.href = context + "/index.do";
+}
+
+
+// 마이페이지 메서드 입니다.
+function goToMyPageView(context) {
+	location.href = context + "/myPageView.do";
+}
