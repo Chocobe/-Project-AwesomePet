35138339f71c34c30c79f0f3d5c912d183be7992
Chocobe <nice_10bird@naver.com>
2020-01-17 18:23:00 +0900

    <master> : Created class "QuestionBoardService.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/service/QuestionBoardService.java
@@ -0,0 +1,33 @@
+package com.awesomePet.service;
+
+import java.util.List;
+
+import com.awesomePet.dao.QuestionBoardDAO;
+import com.awesomePet.vo.QuestionContentsVO;
+
+public class QuestionBoardService {
+	private QuestionBoardDAO questionBoardDAO;
+	
+// 초기화
+	public QuestionBoardService() {
+		questionBoardDAO = new QuestionBoardDAO();
+	}
+	
+	
+// "궁금해요" 게시판의 게시글 총 개수를 구합니다.
+	public int getTotalContentsCnt() {
+		return questionBoardDAO.selectTotalContentsCnt();
+	}
+	
+	
+// "궁금해요" 게시판의 페이지 개수를 구합니다.
+	public int getTotalPageCnt() {
+		return questionBoardDAO.selectTotalPageCnt();
+	}
+	
+	
+// "궁금해요" 게시판의 게시글을 조회합니다.
+	public List<QuestionContentsVO> getQuestionContentsList(int requestPage) {
+		return questionBoardDAO.selectContents(requestPage);
+	}
+}
