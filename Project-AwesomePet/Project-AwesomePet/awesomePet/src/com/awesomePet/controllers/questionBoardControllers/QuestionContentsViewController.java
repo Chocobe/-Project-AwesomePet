cb6f1411677d169d671d7a93fb4219ca8e157df0
Chocobe <nice_10bird@naver.com>
2020-01-21 20:02:23 +0900

    <master> : Created class "QuestionContentsViewController.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionContentsViewController.java
@@ -0,0 +1,28 @@
+package com.awesomePet.controllers.questionBoardControllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.awesomePet.controllers.ControllerUtil;
+import com.awesomePet.controllers.SubController;
+import com.awesomePet.service.QuestionBoardService;
+import com.awesomePet.vo.QuestionContentsVO;
+
+public class QuestionContentsViewController implements SubController {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		String resultPagePath = "/views/questionBoard/questionContentsView.jsp";
+		
+		QuestionBoardService questionBoardService = new QuestionBoardService();
+		int requestBoardIDX = Integer.parseInt(request.getParameter("requestBoardIDX"));
+		
+		QuestionContentsVO questionContentsVO = questionBoardService.getQuestionContents(requestBoardIDX);
+		request.setAttribute("questionContentsVO", questionContentsVO);
+		
+		ControllerUtil.forward(request, response, resultPagePath);
+	}
+}
