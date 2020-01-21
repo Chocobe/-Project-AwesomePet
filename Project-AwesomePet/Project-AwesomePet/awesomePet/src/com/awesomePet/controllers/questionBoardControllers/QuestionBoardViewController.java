df352d2f3c81999b45ad4f856ad38e002bcd0b5a
Chocobe <nice_10bird@naver.com>
2020-01-21 20:01:17 +0900

    <master> : Fixed class "QuestionBoardViewController.java"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionBoardViewController.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionBoardViewController.java
@@ -23,32 +23,33 @@ public class QuestionBoardViewController implements SubController {
 		QuestionBoardVO questionBoardVO = new QuestionBoardVO();
 		
 
-	// 페이지 정보를 구합니다.
-		// 1. 전체 페이지 개수를 구합니다.
+	// 1. 페이지 정보를 구합니다.
+		// 1-1. 전체 페이지 개수를 구합니다.
 		int totalPageCnt = questionBoardService.getTotalPageCnt();
 		
-		// 2. 요청 페이지를 가져옵니다. (요청 페이지 값의 기본값 : 1 페이지)
+		// 1-2. 요청 페이지를 가져옵니다. (요청 페이지 값의 기본값 : 1 페이지)
 		String requestPageString = request.getParameter("requestPage");
 		int requestPage = 1;
 		
-		// 3. 요청한 페이지가 있다면, 해당값을 사용합니다.
-		if(requestPageString != null) {
+		// 1-3. 요청한 페이지가 있다면, 해당값을 사용합니다.
+		if(requestPageString != null && requestPageString.length() > 0) {
 			requestPage = Integer.parseInt(requestPageString);
 		}
 		
-		// 4. 요청 페이지가 총 페이지보다 클 경우는 마지막 페이지로 대체 합니다.
+		// 1-4. 요청 페이지가 총 페이지보다 클 경우는 마지막 페이지로 대체 합니다.
 		if(requestPage > totalPageCnt) {
 			requestPage = totalPageCnt;
 		}
 		
-		// 5. 요청 페이지가 시작 페이지보다 작을 경우는 시작 페이지로 대체합니다.
+		// 1-5. 요청 페이지가 시작 페이지보다 작을 경우는 시작 페이지로 대체합니다.
 		if(requestPage < 1) {
 			requestPage = 1;
 		}
 		
 		questionBoardVO.setPageInfo(totalPageCnt, requestPage);
 		
-		// 2. 요청 페이지에 대한 글 List
+		
+	// 2. 요청 페이지에 대한 글 List
 		List<QuestionContentsVO> questionContentsList = questionBoardService.getQuestionContentsList(requestPage);
 		
 		questionBoardVO.setQuestionContentsList(questionContentsList);
