6a8c0d1c14563059e5155a14d4582fd1621a1271
Chocobe <nice_10bird@naver.com>
2020-01-25 17:16:12 +0900

    <master> : Created class "QuestionContentsUpdateController.java"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionContentsUpdateController.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionContentsUpdateController.java
@@ -7,14 +7,31 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+import com.awesomePet.controllers.ControllerUtil;
 import com.awesomePet.controllers.SubController;
+import com.awesomePet.service.QuestionBoardService;
+import com.awesomePet.vo.QuestionContentsVO;
 
 public class QuestionContentsUpdateController implements SubController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) 
 					throws ServletException, IOException {
 		String resultPagePath = "/questionContentsView.do";
+		QuestionBoardService questionBoardService = new QuestionBoardService();
 		
+		int boardIDX = Integer.parseInt(request.getParameter("requestBoardIDX"));
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
 		
+		QuestionContentsVO questionContentsVO = new QuestionContentsVO(boardIDX,
+																	   title,
+																	   content);
+		int result = questionBoardService.updateQuestionContents(questionContentsVO);
+		
+		if(result == 1) {
+			resultPagePath += "?requestBoardIDX=" + boardIDX + "&action=fixed";
+		}
+		
+		ControllerUtil.forward(request, response, resultPagePath);
 	}
 }
