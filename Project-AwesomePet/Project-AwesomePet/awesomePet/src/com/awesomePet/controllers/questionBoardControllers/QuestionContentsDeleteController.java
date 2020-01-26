fa2b751a5689b4931e1a36a9a3115cda37c8c836
Chocobe <nice_10bird@naver.com>
2020-01-26 18:41:10 +0900

    <master> : Created class "QuestionContentsDeleteController.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionContentsDeleteController.java
@@ -0,0 +1,27 @@
+package com.awesomePet.controllers.questionBoardControllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.awesomePet.controllers.ControllerUtil;
+import com.awesomePet.controllers.SubController;
+import com.awesomePet.service.QuestionBoardService;
+
+public class QuestionContentsDeleteController implements SubController {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		QuestionBoardService questionBoardService = new QuestionBoardService();
+		
+		int requestBoardIDX = Integer.parseInt(request.getParameter("requestBoardIDX"));
+		
+		int result = questionBoardService.deleteQuestionContents(requestBoardIDX);
+		
+		PrintWriter out = response.getWriter();
+		out.print(result);
+	}
+}
