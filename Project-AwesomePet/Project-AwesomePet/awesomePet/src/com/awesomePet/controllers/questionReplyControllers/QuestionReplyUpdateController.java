9b06387819a3df6bd72d0badcf3c7e7a46f27f5e
Chocobe <nice_10bird@naver.com>
2020-01-29 20:48:59 +0900

    <master> : Created class "QuestionReplyUpdateController.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionReplyControllers/QuestionReplyUpdateController.java
@@ -0,0 +1,39 @@
+package com.awesomePet.controllers.questionReplyControllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.awesomePet.controllers.SubController;
+import com.awesomePet.service.QuestionReplyService;
+import com.awesomePet.vo.QuestionReplyContentsVO;
+import com.google.gson.Gson;
+
+public class QuestionReplyUpdateController implements SubController {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		int replyIDX = Integer.parseInt(request.getParameter("replyIDX"));
+		String content = request.getParameter("content");
+		
+		QuestionReplyContentsVO questionReplyContentsVO = new QuestionReplyContentsVO(replyIDX, content);
+		
+		QuestionReplyService questionReplyService = new QuestionReplyService();
+		int result = questionReplyService.updateQuestionReply(questionReplyContentsVO);
+		
+		String resultContents = null;
+		
+		PrintWriter out = response.getWriter();
+		if(result == 1) {
+			QuestionReplyContentsVO resultVO = questionReplyService.getQuestionReply(replyIDX);
+			resultContents = resultVO.getContent();
+		}
+		
+		out.print(resultContents);
+		
+		out.close();
+	}
+}
