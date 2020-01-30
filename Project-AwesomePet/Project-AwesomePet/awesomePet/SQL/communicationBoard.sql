8ca199e9bb029511ca24e597f9893bdc078e27b5
Chocobe <nice_10bird@naver.com>
2020-01-30 20:11:31 +0900

    <master> : Created sql "communicationBoard.sql"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/SQL/communicationBoard.sql
@@ -0,0 +1,241 @@
+CREATE TABLE communicationBoard(
+	boardIDX INTEGER AUTO_INCREMENT,
+	writerID VARCHAR(20),
+	title VARCHAR(50),
+	content VARCHAR(500),
+	
+	imgLocation_1 VARCHAR(500),
+	imgOriginLocation_1 VARCHAR(500),
+	
+	imgLocation_2 VARCHAR(500),
+	imgOriginLocation_2 VARCHAR(500),
+	
+	imgLocation_3 VARCHAR(500),
+	imgOriginLocation_3 VARCHAR(500),
+	
+	writeDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+	writeDate DATE DEFAULT (DATE(writeDateTime)),
+	watch INTEGER DEFAULT 0, 
+	replyCnt INTEGER DEFAULT 0,
+	PRIMARY KEY(boardIDX), 
+	FOREIGN KEY(writerID) REFERENCES awesomepetmember(memberID)
+);
+DROP TABLE communicationboard;
+
+
+SELECT * FROM communicationboard;
+
+SELECT * FROM communicationboard
+ORDER BY boardIDX DESC;
+
+
+-- 테스트 데이터 입력
+-- 01
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('aa',
+										 '소통해요 테스트_1',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('bb',
+										 '소통해요 테스트_2',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('cc',
+										 '소통해요 테스트_3',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('aa',
+										 '소통해요 테스트_4',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('aa',
+										 '소통해요 테스트_5',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('cc',
+										 '소통해요 테스트_6',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('aa',
+										 '소통해요 테스트_7',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('cc',
+										 '소통해요 테스트_8',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('bb',
+										 '소통해요 테스트_9',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('bb',
+										 '소통해요 테스트_10',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
+										 
+INSERT INTO communicationboard(writerID,
+										 title,
+										 content,
+										 imgLocation_1,
+										 imgOriginLocation_1,
+										 imgLocation_2,
+										 imgOriginLocation_2,
+										 imgLocation_3,
+										 imgOriginLocation_3)
+								VALUES('ff',
+										 '소통해요 테스트_11',
+										 '테스트중 입니다.',
+										 '이미지 경로_1',
+										 '원본_1',
+										 '이미지 경로_2',
+										 '원본_2',
+										 '이미지 경로_3',
+										 '원본_3');
