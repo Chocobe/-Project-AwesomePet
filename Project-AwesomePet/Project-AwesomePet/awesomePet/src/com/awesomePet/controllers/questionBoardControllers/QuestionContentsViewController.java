2898ec15c00370421b1eeec550940956a3cdc184
Chocobe <nice_10bird@naver.com>
2020-01-30 19:43:05 +0900

    <master> : Removed alert elements

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionContentsViewController.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionContentsViewController.java
@@ -1,7 +1,6 @@
 package com.awesomePet.controllers.questionBoardControllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
