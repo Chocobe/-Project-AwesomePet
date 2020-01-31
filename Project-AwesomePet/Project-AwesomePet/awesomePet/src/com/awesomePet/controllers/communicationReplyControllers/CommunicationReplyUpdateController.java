9be7623f10e8a0d44cf6f6dded5f3d8ede8b43d6
Chocobe <nice_10bird@naver.com>
2020-01-31 14:13:29 +0900

    <master> : Created class "CommunicationReplyUpdateController.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/communicationReplyControllers/CommunicationReplyUpdateController.java
@@ -0,0 +1,42 @@
+package com.awesomePet.controllers.communicationReplyControllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.awesomePet.controllers.SubController;
+import com.awesomePet.service.CommunicationReplyService;
+import com.awesomePet.service.QuestionReplyService;
+import com.awesomePet.vo.CommunicationReplyContentsVO;
+import com.awesomePet.vo.QuestionReplyContentsVO;
+import com.google.gson.Gson;
+
+public class CommunicationReplyUpdateController implements SubController {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		int replyIDX = Integer.parseInt(request.getParameter("replyIDX"));
+		String content = request.getParameter("content");
+		
+		CommunicationReplyContentsVO communicationReplyContentsVO = 
+						new CommunicationReplyContentsVO(replyIDX, content);
+		
+		CommunicationReplyService communicationReplyService = new CommunicationReplyService();
+		int result = communicationReplyService.updateCommunicationReply(communicationReplyContentsVO);
+		
+		String resultContents = null;
+		
+		PrintWriter out = response.getWriter();
+		if(result == 1) {
+			CommunicationReplyContentsVO resultVO = communicationReplyService.getCommunicationReply(replyIDX);
+			resultContents = resultVO.getContent();
+		}
+		
+		out.print(resultContents);
+		
+		out.close();
+	}
+}
