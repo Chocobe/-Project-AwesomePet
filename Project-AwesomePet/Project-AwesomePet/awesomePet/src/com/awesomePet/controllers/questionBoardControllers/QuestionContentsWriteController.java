be33bed02bd14807fda820b373bfd86c6fe1ccde
Chocobe <nice_10bird@naver.com>
2020-01-31 17:35:13 +0900

    <master> : Fixed alert element

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionContentsWriteController.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/questionBoardControllers/QuestionContentsWriteController.java
@@ -1,7 +1,6 @@
 package com.awesomePet.controllers.questionBoardControllers;
 
 import java.io.IOException;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
