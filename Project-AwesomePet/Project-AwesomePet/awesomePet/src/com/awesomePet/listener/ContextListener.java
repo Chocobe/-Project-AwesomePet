100fc535767626e1b0dfe7e8fec8e36a10fa8e6a
Chocobe <nice_10bird@naver.com>
2020-02-04 16:47:58 +0900

    <master> : Added DI for "CommunicationContentsUpdateController.java"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/listener/ContextListener.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/listener/ContextListener.java
@@ -11,6 +11,7 @@ import com.awesomePet.controllers.communicationBoardControllers.CommunicationBoa
 import com.awesomePet.controllers.communicationBoardControllers.CommunicationContentsAjaxViewController;
 import com.awesomePet.controllers.communicationBoardControllers.CommunicationContentsHitCheckerController;
 import com.awesomePet.controllers.communicationBoardControllers.CommunicationContentsHitController;
+import com.awesomePet.controllers.communicationBoardControllers.CommunicationContentsUpdateController;
 import com.awesomePet.controllers.communicationBoardControllers.CommunicationContentsViewController;
 import com.awesomePet.controllers.communicationBoardControllers.CommunicationContentsWriteController;
 import com.awesomePet.controllers.communicationBoardControllers.CommunicationWriterViewController;
@@ -143,6 +144,9 @@ public class ContextListener implements ServletContextListener {
 		
 		// 자유 게시판(소통해요) 특정 글 페이지 JSON으로 요청 컨트롤러
 		subControllers.put("/communicationContentsAjaxView.do", new CommunicationContentsAjaxViewController());
+		
+		// 자유 게시판(소통해요) 특정 수정 요청 컨트롤러
+		subControllers.put("/communicationContentsUpdate.do", new CommunicationContentsUpdateController());
 		
 		// 자유 게시판(소통해요) "좋아요" 여부 조회 컨트롤러
 		subControllers.put("/communicationContentsHitChecker.do", new CommunicationContentsHitCheckerController());
