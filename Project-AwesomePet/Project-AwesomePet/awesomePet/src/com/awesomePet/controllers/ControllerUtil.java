aded37c18e6d633dc5b507b8b3d58b21e6745b18
Chocobe <nice_10bird@naver.com>
2020-02-04 16:47:09 +0900

    <master> : Added static method "removeImgFile()"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/ControllerUtil.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/ControllerUtil.java
@@ -1,5 +1,6 @@
 package com.awesomePet.controllers;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -20,5 +21,33 @@ public class ControllerUtil {
 	public static void redirect(HttpServletResponse response, String path) 
 					throws ServletException, IOException {
 		response.sendRedirect(path);
+	}
+	
+	
+	// SubController 객체에서 파일 삭제를 위한 유틸 메서드 입니다.
+	public static void removeImgFile(HttpServletRequest request, String folderName, String imgLocation) {
+		if(imgLocation != null && imgLocation.length() > 0) {
+			String folderPath = request.getContextPath() + "/" + folderName + "/";
+			int folderPathLength = folderPath.length();
+			
+			String imgFileName = imgLocation.substring(folderPathLength);
+			String realPath = request.getServletContext().getRealPath(folderName) + "/" + imgFileName;
+			
+			File imgFile = new File(realPath);
+			
+			System.out.println("\n--- imgLocation값 : " + imgLocation);
+			System.out.println("--- folderPath 값 : " + folderPath);
+			System.out.println("--- imgFileName : " + imgFileName);
+			
+			System.out.println("--- realPath 값 : " + realPath + "\n");
+			
+			if(imgFile.exists()) {
+				imgFile.delete();
+				System.out.println("--- 이미지 삭제 완료");
+				
+			} else {
+				System.out.println("--- 파일을 찾을 수 없습니다");
+			}
+		}
 	}
 }
