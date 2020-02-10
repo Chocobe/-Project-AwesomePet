91940adcf7cc16e1f86717c2214e2ba74655badd
Chocobe <nice_10bird@naver.com>
2020-02-10 17:06:46 +0900

    <master> : Created class "PetBoardImageVO.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/vo/PetBoardImageVO.java
@@ -0,0 +1,58 @@
+package com.awesomePet.vo;
+
+public class PetBoardImageVO {
+	private int boardIDX;
+	private int orderNumber;
+	private String imgLocation;
+	private String imgOriginLocation;
+	
+	
+// 생성자
+	public PetBoardImageVO() { }
+	
+	public PetBoardImageVO(int boardIDX,
+							int orderNumber,
+							String imgLocation,
+							String imgOriginLocation) {
+		this.boardIDX = boardIDX;
+		this.orderNumber = orderNumber;
+		this.imgLocation = imgLocation;
+		this.imgOriginLocation = imgOriginLocation;
+	}
+
+	
+// boardIDX
+	public int getBoardIDX() {
+		return boardIDX;
+	}
+	public void setBoardIDX(int boardIDX) {
+		this.boardIDX = boardIDX;
+	}
+
+	
+// orderNumber
+	public int getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(int orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	
+// imgLocation
+	public String getImgLocation() {
+		return imgLocation;
+	}
+	public void setImgLocation(String imgLocation) {
+		this.imgLocation = imgLocation;
+	}
+
+	
+// imgOriginLocation
+	public String getImgOriginLocation() {
+		return imgOriginLocation;
+	}
+	public void setImgOriginLocation(String imgOriginLocation) {
+		this.imgOriginLocation = imgOriginLocation;
+	}
+}
