b49b67891a4b255b1e33fbf5855fedc380b0b0aa
Chocobe <nice_10bird@naver.com>
2020-02-10 20:46:19 +0900

    <master> : Added attr "originTypeName", "originSubTypeName"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/vo/PetSubTypeVO.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/vo/PetSubTypeVO.java
@@ -2,7 +2,11 @@ package com.awesomePet.vo;
 
 public class PetSubTypeVO {
 	private String typeName;
+	private String originTypeName;
+	
 	private String subTypeName;
+	private String originSubTypeName;
+	
 	private String subTypeComment;
 	
 	
@@ -12,8 +16,22 @@ public class PetSubTypeVO {
 	public PetSubTypeVO(String typeName,
 						String subTypeName,
 						String subTypeComment) {
+		this(typeName,
+			 null,
+			 subTypeName,
+			 null,
+			 subTypeComment);
+	}
+	
+	public PetSubTypeVO(String typeName,
+						String originTypeName,
+						String subTypeName,
+						String originSubTypeName,
+						String subTypeComment) {
 		this.typeName = typeName;
+		this.originTypeName = originTypeName;
 		this.subTypeName = subTypeName;
+		this.originSubTypeName = originSubTypeName;
 		this.subTypeComment = subTypeComment;
 	}
 	
@@ -27,6 +45,15 @@ public class PetSubTypeVO {
 	}
 	
 	
+// originTypeName
+	public void setOriginTypeName(String originTypeName) {
+		this.originTypeName = originTypeName;
+	}
+	public String getOriginTypeName() {
+		return originTypeName;
+	}
+	
+	
 // subTypeName
 	public void setSubTypeName(String subTypeName) {
 		this.subTypeName = subTypeName;
@@ -35,6 +62,15 @@ public class PetSubTypeVO {
 		return subTypeName;
 	}
 	
+	
+// originSubTypeName
+	public void setOriginSubTypeName(String originSubTypeName) {
+		this.originSubTypeName = originSubTypeName;
+	}
+	public String getOriginSubTypeName() {
+		return originSubTypeName;
+	}
+
 	
 // subTypeComment
 	public void setSubTypeComment(String subTypeComment) {
