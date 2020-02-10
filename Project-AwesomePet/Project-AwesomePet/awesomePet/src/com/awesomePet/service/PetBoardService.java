a927b7662302c7e8f8bb2e2daf9f0769f6374dd9
Chocobe <nice_10bird@naver.com>
2020-02-10 17:10:25 +0900

    <master> : Added methods

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/service/PetBoardService.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/service/PetBoardService.java
@@ -3,7 +3,10 @@ package com.awesomePet.service;
 import java.util.List;
 
 import com.awesomePet.dao.PetBoardDAO;
+import com.awesomePet.vo.PetBoardImageVO;
+import com.awesomePet.vo.PetBoardVO;
 import com.awesomePet.vo.PetTypeVO;
+import com.awesomePet.vo.PetVO;
 
 public class PetBoardService {
 	private PetBoardDAO petBoardDAO;
@@ -18,5 +21,29 @@ public class PetBoardService {
 // petType 의 전체 데이터를 조회 합니다.
 	public List<PetTypeVO> getPetTypeList() {
 		return petBoardDAO.selectTotalPetType();
+	}
+	
+	
+// pet 테이블에 데이터를 INSERT 합니다.
+	public int writePet(PetVO petVO) {
+		return petBoardDAO.insertPet(petVO);
+	}
+	
+	
+// pet 테이블에서 petID를 SELECT 합니다.
+	public int getPetID(PetVO petVO) {
+		return petBoardDAO.selectPetID(petVO);
+	}
+	
+	
+// petBoard 테이블에 데이터를 INSERT 합니다.
+	public int writePetBoard(PetBoardVO petBoardVO) {
+		return petBoardDAO.insertPetBoard(petBoardVO);
+	}
+	
+	
+// petBoardImage 테이블에 데이터를 INSERT 합니다.
+	public int writePetBoardImages(List<PetBoardImageVO> petBoardImageList) {
+		return petBoardDAO.insertPetBoardImages(petBoardImageList);
 	}
 }
