e33e1f05031ffb3e4a54ab08de172f514fb1a99a
Chocobe <nice_10bird@naver.com>
2020-02-10 17:09:05 +0900

    <master> : Created class "PetBoardVO.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/vo/PetBoardVO.java
@@ -0,0 +1,82 @@
+package com.awesomePet.vo;
+
+import java.time.LocalDate;
+
+public class PetBoardVO {
+	private int boardIDX;
+	private String writerID;
+	private int watch;
+	private LocalDate writeDate;
+	private String boardState;
+	
+	
+// 생성자
+	public PetBoardVO() { }
+	
+	public PetBoardVO(int boardIDX,
+					  String writerID,
+					  String boardState) {
+		this(boardIDX,
+			 writerID,
+			 0,
+			 null,
+			 boardState);
+	}
+	
+	public PetBoardVO(int boardIDX,
+					  String writerID,
+					  int watch,
+					  LocalDate writeDate,
+					  String boardState) {
+		this.boardIDX = boardIDX;
+		this.writerID = writerID;
+		this.watch = watch;
+		this.writeDate = writeDate;
+		this.boardState = boardState;
+	}
+
+	
+// boardIDX
+	public int getBoardIDX() {
+		return boardIDX;
+	}
+	public void setBoardIDX(int boardIDX) {
+		this.boardIDX = boardIDX;
+	}
+
+	
+// writerID
+	public String getWriterID() {
+		return writerID;
+	}
+	public void setWriterID(String writerID) {
+		this.writerID = writerID;
+	}
+
+	
+// watch
+	public int getWatch() {
+		return watch;
+	}
+	public void setWatch(int watch) {
+		this.watch = watch;
+	}
+
+	
+// writeDate
+	public LocalDate getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(LocalDate writeDate) {
+		this.writeDate = writeDate;
+	}
+
+	
+// boardState
+	public String getBoardState() {
+		return boardState;
+	}
+	public void setBoardState(String boardState) {
+		this.boardState = boardState;
+	}
+}
