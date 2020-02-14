8667f36a523c68be652b72be6d84dc5f015ef66f
Chocobe <nice_10bird@naver.com>
2020-02-15 05:59:46 +0900

    <master> : Created class "PetContentsDeleteController.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/controllers/communicationBoardControllers/PetContentsDeleteController.java
@@ -0,0 +1,33 @@
+package com.awesomePet.controllers.communicationBoardControllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.awesomePet.controllers.SubController;
+import com.awesomePet.service.PetBoardService;
+
+public class PetContentsDeleteController implements SubController {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		int requestBoardIDX = Integer.parseInt(request.getParameter("requestBoardIDX"));
+		
+		PetBoardService petBoardService = new PetBoardService();
+		int result = petBoardService.deletePetContents(requestBoardIDX);
+		
+		PrintWriter out = response.getWriter();
+		
+		if(result == 1) {
+			out.print("true");
+			
+		} else {
+			out.print("false");
+		}
+		
+		out.close();
+	}
+}
