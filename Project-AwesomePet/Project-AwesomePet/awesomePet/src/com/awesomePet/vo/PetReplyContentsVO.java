d39ac545869223ea38f8a5748a640046e9df7d99
Chocobe <nice_10bird@naver.com>
2020-02-15 16:42:29 +0900

    <master> : Created class "PetReplyContentsVO.java"

---
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/vo/PetReplyContentsVO.java
@@ -0,0 +1,91 @@
+package com.awesomePet.vo;
+
+import java.time.LocalDate;
+
+public class PetReplyContentsVO {
+	private int replyIDX;
+	private int parentIDX;
+	private String writerID;
+	private String content;
+	private LocalDate writeDate;
+
+	
+// 생성자
+	public PetReplyContentsVO() { }
+
+	public PetReplyContentsVO(int replyIDX,
+							  String content) {
+		this(replyIDX,
+			 -1,
+			 null,
+			 content,
+			 null);
+	}
+	
+	public PetReplyContentsVO(int parentIDX,
+							  String writerID,
+							  String content) {
+		this(-1,
+			 parentIDX,
+			 writerID,
+			 content,
+			 null);
+	}
+	
+	public PetReplyContentsVO(int replyIDX,
+							  int parentIDX,
+							  String writerID,
+							  String content,
+							  LocalDate writeDate) {
+		this.replyIDX = replyIDX;
+		this.parentIDX = parentIDX;
+		this.writerID = writerID;
+		this.content = content;
+		this.writeDate = writeDate;
+	}
+	
+	
+// replyIDX
+	public int getReplyIDX() {
+		return replyIDX;
+	}
+	public void setReplyIDX(int replyIDX) {
+		this.replyIDX = replyIDX;
+	}
+
+	
+// parentIDX
+	public int getParentIDX() {
+		return parentIDX;
+	}
+	public void setParentIDX(int parentIDX) {
+		this.parentIDX = parentIDX;
+	}
+
+	
+// writerID
+	public String getWriterID() {
+		return writerID;
+	}
+	public void setWriterID(String writerID) {
+		this.writerID = writerID;
+	}
+
+	
+// content
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	
+// writeDate
+	public LocalDate getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(LocalDate writeDate) {
+		this.writeDate = writeDate;
+	}
+}
