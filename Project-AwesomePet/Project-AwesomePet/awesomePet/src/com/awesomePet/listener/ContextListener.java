b636b47f9ae1acc3c6781d83d870997906adf817
Chocobe <nice_10bird@naver.com>
2020-02-15 17:01:55 +0900

    <master> : Added DI for "PetReplyDeleteController.java"

--- Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/listener/ContextListener.java
+++ Project-AwesomePet/Project-AwesomePet/awesomePet/src/com/awesomePet/listener/ContextListener.java
@@ -46,6 +46,7 @@ import com.awesomePet.controllers.petBoardController.PetTypeDeleteController;
 import com.awesomePet.controllers.petBoardController.PetTypeUpdateController;
 import com.awesomePet.controllers.petBoardController.PetTypeViewController;
 import com.awesomePet.controllers.petBoardController.PetTypeWriteController;
+import com.awesomePet.controllers.petReplyController.PetReplyDeleteController;
 import com.awesomePet.controllers.petReplyController.PetReplyUpdateController;
 import com.awesomePet.controllers.petReplyController.PetReplyViewController;
 import com.awesomePet.controllers.petReplyController.PetReplyWriteController;
@@ -236,6 +237,7 @@ public class ContextListener implements ServletContextListener {
 		subControllers.put("/petReplyUpdate.do", new PetReplyUpdateController());
 		
 		// 자유 게시판(소통해요)의 "댓글" 삭제 요청 컨트롤러
+		subControllers.put("/petReplyDelete.do", new PetReplyDeleteController());
 		
 		
 		
